e00af114f8a91622a074e0813f6db747133427ca
saatvikkher <saatvikkher1@gmail.com>
2023-06-30 11:41:55 -0400

    added 5 start icp

--- solepair.py
+++ solepair.py
@@ -60,20 +60,97 @@ class SolePair():
         return transformed_q
     
     
-    def icp_transform(self, max_iterations: int = 10000, tolerance: float = 0.00001, downsample_rate: float = 1.0, random_seed: int = 47) -> pd.DataFrame | pd.DataFrame:
-        q_pts, k_pts = self._equalize()
+    def icp_transform(self, max_iterations: int = 10000, 
+                      tolerance: float = 0.00001, 
+                      downsample_rate: float = 1.0, 
+                      random_seed: int = 47, 
+                      shift_left=False, 
+                      shift_right=False, 
+                      shift_up=False, 
+                      shift_down=False) -> pd.DataFrame | pd.DataFrame:
+        '''
+        Default: do 1 icp with no initial shift
+        The user can specify up to four additional icp trials with differnt initial shifts
+        '''
+        print("Q coords before: ", self.Q.coords)
 
+        # Default no shift
+        shifts = [(0, 0)]
+        
+
+        range = 2 * max((self.Q.coords.x.max() - self.Q.coords.x.min()), (self.Q.coords.y.max() - self.Q.coords.y.min()))
+        print("range: ", range)
+        
+        if shift_left:
+            shifts.append((-range, 0))
+        if shift_right:
+            shifts.append((range, 0))
+        if shift_down:
+            shifts.append((0, -range))
+        if shift_up:
+            shifts.append((0, range))
+        
+        print("SHIFT: ", shifts)
+
+        best_T = None
+        best_rmse = np.Inf 
+        best_shift = None
+
+        for shift in shifts:
+            # apply shift
+            self.Q.coords.loc[:,"x"] += shift[0]
+            self.Q.coords.loc[:,"y"] += shift[1]
+            
+            T, rmse = self._icp_helper(max_iterations=max_iterations, tolerance=tolerance, downsample_rate=downsample_rate, random_seed=random_seed)
+            print("RMSE calculation: ", rmse)
+            if rmse < best_rmse:
+                best_rmse = rmse
+                best_T = T
+                best_shift = shift
+                print("Best RMSE: ", best_rmse)
+
+            # reverse shift
+            self.Q.coords.loc[:,"x"] -= shift[0]
+            self.Q.coords.loc[:,"y"] -= shift[1]
+        
+        self.T = best_T
+
+        print("Q coords: ", self.Q.coords)
+
+        # Apply the best_shift
+        self.Q.coords.loc[:,"x"] += best_shift[0]
+        self.Q.coords.loc[:,"y"] += best_shift[1]
+        
+        transformed_q = pd.DataFrame(self._transform(self.Q.coords, self.T))
+        self.Q.aligned_coordinates = transformed_q.rename(columns = {0: 'x', 1 : 'y'})
+        self.aligned = True
+
+        # reverse best_shift
+        self.Q.coords.loc[:,"x"] -= best_shift[0]
+        self.Q.coords.loc[:,"y"] -= best_shift[1]
+
+        return self.Q.aligned_coordinates, self.K.coords
+
+
+    def _icp_helper(self, max_iterations: int = 10000, 
+                      tolerance: float = 0.00001, 
+                      downsample_rate: float = 1.0, 
+                      random_seed: int = 47):
+        
+        q_pts, k_pts = self._equalize()
+        np.random.seed(random_seed)
         num_samples = int(q_pts.shape[0] * downsample_rate)
         sample_indices_q = np.random.choice(q_pts.shape[0], num_samples, replace=False)
         sample_indices_k = np.random.choice(k_pts.shape[0], num_samples, replace=False)
         q_pts = q_pts[sample_indices_q]
         k_pts = k_pts[sample_indices_k]
 
-        self.T,_,i = icp(q_pts,k_pts, max_iterations=max_iterations, tolerance=tolerance)
-        transformed_q = pd.DataFrame(self._transform(self.Q.coords, self.T))
-        self.Q.aligned_coordinates = transformed_q.rename(columns = {0: 'x', 1 : 'y'})
-        self.aligned = True
-        return self.Q.aligned_coordinates, self.K.coords
+        T, distances, _ = icp(q_pts,k_pts, max_iterations=max_iterations, tolerance=tolerance)
+
+        # RMSE (Root Mean Squared Error) calc
+        rmse = np.sqrt(np.mean(distances)**2)
+
+        return T, rmse
     
     def plot(self, size: float = 0.5, aligned: bool = False):
         '''
--- solepaircompare.py
+++ solepaircompare.py
@@ -8,6 +8,7 @@ import time
 from solepair import SolePair
 from sole import Sole
 
+
 class SolePairCompare:
     '''
     SolePairCompare will take two shoeprint images (Q and K) and eventually 
@@ -15,15 +16,23 @@ class SolePairCompare:
 
     This class also provides methods for intermediate steps in this pipeline.
     '''
-    def __init__(self, pair: SolePair, downsample_rate=0.2, icp_downsample_rate=1.0, random_seed=47) -> None:
+
+    def __init__(self, pair: SolePair, downsample_rate=0.2, icp_downsample_rate=1.0, random_seed=47,
+                 shift_left=False,
+                 shift_right=False,
+                 shift_up=False,
+                 shift_down=False) -> None:
         '''
         Inputs:
             Q: (Pandas DataFrame) for shoe Q
             K: (Pandas DataFrame) for shoe K
         '''
-        self.Q_coords, self.K_coords = pair.icp_transform(downsample_rate=icp_downsample_rate)
-        self.Q_coords = self.Q_coords.sample(frac=downsample_rate, random_state=random_seed)
-        self.K_coords = self.K_coords.sample(frac=downsample_rate, random_state=random_seed)
+        self.Q_coords, self.K_coords = pair.icp_transform(
+            downsample_rate=icp_downsample_rate, shift_left=shift_left, shift_right=shift_right, shift_up=shift_up, shift_down=shift_down)
+        self.Q_coords = self.Q_coords.sample(
+            frac=downsample_rate, random_state=random_seed)
+        self.K_coords = self.K_coords.sample(
+            frac=downsample_rate, random_state=random_seed)
         self.pair = pair
 
     def _df_to_hash(self, df):
@@ -31,14 +40,14 @@ class SolePairCompare:
         Changes a dataframe to a hashtable.
 
         Inputs: df (dataframe)
-        
+
         Returns: (defaultdict)
         '''
         hashtable = defaultdict(set)
         for _, row in df.iterrows():
             # We assume the column names are "x" and "y"
             hashtable[row['x']].add(row['y'])
-        return hashtable 
+        return hashtable
 
     def _is_overlap(self, x, y, ht, threshold):
         '''
@@ -80,7 +89,7 @@ class SolePairCompare:
         Returns: proportion of points that overlap (float)
         '''
 
-        # Round Q and K coords because the hashtable implementation in 
+        # Round Q and K coords because the hashtable implementation in
         # _df_to_hash relies on integer values of coordinates
         if Q_as_base:
             df1 = self.Q_coords.round().astype(int)
@@ -88,12 +97,12 @@ class SolePairCompare:
         else:
             df1 = self.K_coords.round().astype(int)
             df2 = self.Q_coords.round().astype(int)
-            
+
         ht = self._df_to_hash(df2)
-        overlap_count = df1.apply(lambda point: self._is_overlap(point['x'], 
-                                    point['y'], ht, threshold), axis=1).sum()
+        overlap_count = df1.apply(lambda point: self._is_overlap(point['x'],
+                                                                 point['y'], ht, threshold), axis=1).sum()
         return overlap_count/len(df1)
-    
+
     def min_dist(self, Q_as_base=True):
         '''
         (Similarity metric) 
@@ -102,19 +111,19 @@ class SolePairCompare:
         For each sample point from the base dataframe, min_dist finds the Euclidean 
         distance of the closest point in the other dataframe using the 
         kdtree closest neighbor algorithm. 
-        
+
         It outputs distribution statistics including the average, 
         standard deviation, and 0.1, 0.25, 0.5, 0.75, 0.9 quantile values, as well as
         kurtosis.
 
         Inputs:
             Q_as_base (bool): if Q is the base dataframe, defaults to True
-        
+
         Returns:
             (dict): a dictionary of statistics with keys "mean", "std", "0.1", 
                     "0.25", "0.5", "0.75", "0.9", "kurtosis"
         '''
-        
+
         # If Q_as_base == True, Q is the shoe we sample from
         if Q_as_base:
             df1 = self.K_coords
@@ -124,17 +133,17 @@ class SolePairCompare:
             df2 = self.K_coords
 
         min_dists = []
-        
+
         # We assume the column names are "x" and "y"
         points = df1[["x", "y"]].values
         kdtree = KDTree(points)
         query_points = df2[['x', 'y']].values
-        
+
         for query_point in query_points:
             dist, _ = kdtree.query(query_point)
             min_dists.append(dist)
-        
-        min_dists_arr = np.array(min_dists)   
+
+        min_dists_arr = np.array(min_dists)
         min_dists_dict = {}
         min_dists_dict["mean"] = np.mean(min_dists_arr)
         min_dists_dict["std"] = np.std(min_dists_arr)
@@ -144,5 +153,5 @@ class SolePairCompare:
         min_dists_dict["0.75"] = np.quantile(min_dists_arr, 0.75)
         min_dists_dict["0.9"] = np.quantile(min_dists_arr, 0.9)
         min_dists_dict["kurtosis"] = kurtosis(min_dists_arr)
-        
+
         return min_dists_dict
