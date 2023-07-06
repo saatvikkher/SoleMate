97d1ee17a5c61ba4f5aef2fcbdf8767c0f74bff9
saatvikkher <saatvikkher1@gmail.com>
2023-07-06 01:13:39 -0400

    cleaned up

--- util.py
+++ util.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 
 METADATA = pd.read_csv("2DScanImages/Image-Info.csv")
-BLACK_WHITE_THRESHOLD = 85 # Bottom third of grayscale: 255 / 3 = 85
+BLACK_WHITE_THRESHOLD = 85  # Bottom third of grayscale: 255 / 3 = 85
 WILLIAMS_PURPLE = "#500082"
 WILLIAMS_GOLD = "#FFBE0A"
+
 
 def _create_km_pairs(df, name: str):
     '''
@@ -12,21 +13,22 @@ def _create_km_pairs(df, name: str):
     '''
 
     kms = []
-    
-    df = df.sort_values(by = 'Shoe Number')
+
+    df = df.sort_values(by='Shoe Number')
     for i in range(len(df)):
-        for j in range(i+1,len(df)):
+        for j in range(i+1, len(df)):
             if (df.iloc[i].loc['Shoe Number'] == df.iloc[j].loc['Shoe Number'] and
-                df.iloc[i].loc['Foot'] == df.iloc[j].loc['Foot'] and
-                df.iloc[i].loc['Image Number'] == df.iloc[j].loc['Image Number'] and
-                df.iloc[i].loc['Visit Number'] == df.iloc[j].loc['Visit Number'] and
-                df.iloc[i].loc['Shoe Size'] == df.iloc[j].loc['Shoe Size']
-            ) :
-                kms.append((df.iloc[i].loc['File Name'], df.iloc[j].loc['File Name']))
+                    df.iloc[i].loc['Foot'] == df.iloc[j].loc['Foot'] and
+                    df.iloc[i].loc['Image Number'] == df.iloc[j].loc['Image Number'] and
+                    df.iloc[i].loc['Visit Number'] == df.iloc[j].loc['Visit Number'] and
+                    df.iloc[i].loc['Shoe Size'] == df.iloc[j].loc['Shoe Size']
+                ) :
+                kms.append((df.iloc[i].loc['File Name'],
+                           df.iloc[j].loc['File Name']))
                 break
 
     kms_df = pd.DataFrame(kms)
-    kms_df.rename(columns = {0: 'q', 1 : 'k'}, inplace=True)
+    kms_df.rename(columns={0: 'q', 1: 'k'}, inplace=True)
     kms_df.to_csv(name, index=False)
 
 
@@ -37,7 +39,6 @@ def _create_knm_pairs(df, name: str, size: int = 2407):
     knms = []
 
     for i in range(len(df)):
-        print(i)
         non_mated_row = np.random.randint(len(df))
 
         while (df.iloc[i].loc['File Name'] == df.iloc[non_mated_row].loc['File Name'] or
@@ -46,10 +47,11 @@ def _create_knm_pairs(df, name: str, size: int = 2407):
                df.iloc[i].loc['Shoe Make/Model'] != df.iloc[non_mated_row].loc['Shoe Make/Model'] or
                df.iloc[i].loc['Foot'] != df.iloc[non_mated_row].loc['Foot']):
             non_mated_row = np.random.randint(len(df))
-        
-        knms.append((df.iloc[i].loc['File Name'], df.iloc[non_mated_row].loc['File Name']))
+
+        knms.append((df.iloc[i].loc['File Name'],
+                    df.iloc[non_mated_row].loc['File Name']))
 
     knms_df = pd.DataFrame(knms).sample(size)
     knms_df = knms_df.reset_index(drop=True)
-    knms_df.rename(columns = {0: 'q', 1 : 'k'}, inplace=True)
+    knms_df.rename(columns={0: 'q', 1: 'k'}, inplace=True)
     knms_df.to_csv(name, index=False)
