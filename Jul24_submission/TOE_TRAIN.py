9e8b516dafd393fd5bac0122e4a1b485be491a32
Yufeng Wu <sw20@williams.edu>
2023-07-25 16:20:12 -0400

    idk what changed

--- Jul24_submission/TOE_TRAIN.py
+++ Jul24_submission/TOE_TRAIN.py
@@ -5,13 +5,13 @@ import gc
 import time
 
 # Redirect the standard output to the log file
-sys.stdout = open('./TOE_TEST.log', 'w')
+sys.stdout = open('./TOE_TRAIN.log', 'w')
 
-print("[TOE TEST] Started.")
+print("[TOE TRAIN] Started.")
 
 # Read in testing split
-km_test = pd.read_csv("Jul24_submission/pair_info/BASELINE_TEST_KM.csv")
-knm_test = pd.read_csv("Jul24_submission/pair_info/BASELINE_TEST_KNM.csv")
+km_test = pd.read_csv("Jul24_submission/pair_info/BASELINE_TRAIN_KM.csv")
+knm_test = pd.read_csv("Jul24_submission/pair_info/BASELINE_TRAIN_KNM.csv")
 
 combined_test = pd.concat([km_test, knm_test], ignore_index=True)
 
@@ -25,16 +25,16 @@ df = pd.DataFrame()
 for i in range(len(combined_test)):
     start = time.time()
     if i % 20 == 0:
-        print("[TOE TEST] Progress: ", (i*100) / len(combined_test))
+        print("[TOE TRAIN] Progress: ", (i*100) / len(combined_test))
     try:
         row = pd.DataFrame(process_image(
             Q_files[i], K_files[i], mated[i], partial_type="toe"), index=[0])
         df = pd.concat([df, row], ignore_index=True)
-        df.to_csv("RESULT_TOE_TEST.csv", index=False)
+        df.to_csv("RESULT_TOE_TRAIN.csv", index=False)
     except Exception as e:
-        print("[TOE TEST] Caught error at index " + str(i) + str(e))
+        print("[TOE TRAIN] Caught error at index " + str(i) + str(e))
     end = time.time()
-    print("[TOE TEST]: Time for iteration ", str(i), ": ", str(end - start))
+    print("[TOE TRAIN]: Time for iteration ", str(i), ": ", str(end - start))
     gc.collect()
 
-print("[TOE TEST] Complete!")
+print("[TOE TRAIN] Complete!")
